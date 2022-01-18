51fc0ed1249f37d6904d1f234b0c656aae9609a2
crypted <sami.mhatre756@gmail.com>
2022-01-18 17:09:47 +0530

    Revert "sdm845-common: Nuke PocketMode, LiveDisplay & Trust"
    This reverts commit a4fee15834929a824654c698bf96288655a00f5f.
    
    This also add back LiveDisplay

--- common.mk
+++ common.mk
@@ -155,6 +155,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.oneplus_sdm845
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    lineage.livedisplay@2.0-service.oneplus_sdm845
+
 # QTI Bluetooth
 PRODUCT_PACKAGES += \
     BluetoothQti
